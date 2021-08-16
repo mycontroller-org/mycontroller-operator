e80804baffd2b3233c1bdc6c16e690c87d18dd8e
Jeeva Kandasamy <jkandasa@gmail.com>
2021-08-16 13:10:32 +0530

    update catalog source needs
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- controllers/mycontroller_controller.go
+++ controllers/mycontroller_controller.go
@@ -49,6 +49,11 @@ type MyControllerReconciler struct {
 //+kubebuilder:rbac:groups=mycontroller.org,resources=mycontrollers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=mycontroller.org,resources=mycontrollers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=mycontroller.org,resources=mycontrollers/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=route.openshift.io,resources=routes,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
